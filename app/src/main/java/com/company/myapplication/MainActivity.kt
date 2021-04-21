c7dc39756ed57dc720430d7265c1f473f6471be0
Acer <kozhamseitova91@gmail.com>
2021-04-21 15:45:50 +0600

    auth registration interface

--- app/src/main/java/com/company/myapplication/MainActivity.kt
+++ app/src/main/java/com/company/myapplication/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.company.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.next)
+        button.setOnClickListener{
+            val intent = Intent(this, AuthorizationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
